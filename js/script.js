f21d72ef778e81a0a21181a6d4d641c285e67607
hevils <hevils.crow@mail.ru>
2023-12-23 17:13:13 +0300

    added functionality to filter button

--- js/script.js
+++ js/script.js
@@ -11,10 +11,19 @@ srchInput.addEventListener('focus', toggleFocus);
 srchInput.addEventListener('blur', toggleFocus);
 
 
-
 //resets default link behavior
 let allLinks = document.querySelectorAll('a');
 
 for(let link of allLinks) {
   link.addEventListener('click', (e) => {e.preventDefault();})
 }
+
+
+//adds functionality to the button
+let filterBtn = document.querySelector('.filter-search__button');
+let filter = document.querySelector('.filter');
+
+function showFilter () {
+  filter.classList.add('active');
+}
+filterBtn.addEventListener('click', showFilter);
