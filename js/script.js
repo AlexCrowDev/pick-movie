467d166c6a695a21e6c1a909e8094e5e72db7956
hevils <hevils.crow@mail.ru>
2023-12-23 18:22:13 +0300

    added sidebar

--- js/script.js
+++ js/script.js
@@ -19,11 +19,34 @@ for(let link of allLinks) {
 }
 
 
-//adds functionality to the button
+//adds functionality to filter button
 let filterBtn = document.querySelector('.filter-search__button');
 let filter = document.querySelector('.filter');
 
 function showFilter () {
-  filter.classList.add('active');
+  filter.classList.add('active')
 }
 filterBtn.addEventListener('click', showFilter);
+
+
+//adds sidebar opening functionality
+let mainBtns = document.querySelectorAll('.main-show__button');
+let sideBar = document.querySelector('.sidebar');
+
+function openSidebar () {
+  sideBar.classList.add('open')
+}
+
+for (let btn of mainBtns) {
+  btn.addEventListener('click', openSidebar)
+}
+
+
+//adds didebar closing functionality
+let sidebarBtn = document.querySelector('.sidebar__button');
+
+function closeSidebar() {
+  sideBar.classList.remove('open')
+}
+
+sidebarBtn.addEventListener('click', closeSidebar)
