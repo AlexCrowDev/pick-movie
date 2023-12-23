9b21d4b7aab4073490d057af5bf9e0d54218d880
hevils <hevils.crow@mail.ru>
2023-12-23 16:46:05 +0300

    added the change search bg when its in focus

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 "use strict"
 
-  
+//changes search background when in focus
+let srchInput = document.querySelector('.search-header__input');
+
+function toggleFocus () {
+  srchInput.parentElement.classList.toggle('focused')
+}
+
+srchInput.addEventListener('focus', toggleFocus);
+srchInput.addEventListener('blur', toggleFocus);
+
+
