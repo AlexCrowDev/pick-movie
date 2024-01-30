dfc3f6a56c917e76a4e55d8f5f5f5593436037be
hevils <hevils.crow@mail.ru>
2024-01-30 20:26:38 +0300

    changed CICD

--- .cicd/publish.sh
+++ .cicd/publish.sh
@@ -2,11 +2,10 @@
 set -eu
 
 SITE_DIR='src'
-PROJECT='film-filter'
+PROJECT='kino-poisk'
 
 echo Moving headers
 mv _headers "$SITE_DIR"
 
 echo Publishing
-# npx wrangler pages deploy "$SITE_DIR" --project-name "$PROJECT" --branch "$CI_COMMIT_BRANCH" --commit-message "$CI_COMMIT_MESSAGE"
-npx wrangler pages deploy "$SITE_DIR" --project-name "$PROJECT" --branch "test" --commit-message "$CI_COMMIT_MESSAGE"
+npx wrangler pages deploy "$SITE_DIR" --project-name "$PROJECT" --branch "$CI_COMMIT_BRANCH" --commit-message "$CI_COMMIT_MESSAGE"
