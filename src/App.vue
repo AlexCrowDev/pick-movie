982825779d167110234ef4e12686c51014bd0d54
hevils <hevils.crow@mail.ru>
2024-02-05 14:37:43 +0300

    Init files

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="app">
+    </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style>
+*,
+*::before,
+*::after {
+  padding: 0;
+  margin: 0;
+  border: 0;
+  box-sizing: border-box;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+html, body {
+  height: 100%;
+}
+#app {
+  height: 100%;
+}
+.app {
+  height: 100%;
+  line-height: 1;
+  background-color: #16141c;
+  font-size: 14px;
+  color: #fff;
+  font-family: 'Comfortaa', sans-serif;
+  letter-spacing: 0.3px;
+}
+._container {
+  padding: 1px 15px;
+  margin: 0px auto;
+  box-sizing: content-box;
+}
+.item {
+  background-color: rgb(91 87 107 / 20%);
+  margin: 0 0 15px;
+  border-radius: 10px;
+}
+</style>
